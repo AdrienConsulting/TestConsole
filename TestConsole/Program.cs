9f2434ec977d61eb3573a6b80c0fbf59d60537a2
Adrien <Adrien.goncalves@mantaconsulting.fr>
2024-10-09 12:01:55 +0200

    Premier commit

---
+++ TestConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 9;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Adrien");
+            Console.ReadKey();
+        }
+    }
+}
